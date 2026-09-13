3cbf5bdba03df1a3f0db007a9893bb606c23af7a
elysianarchitekts-lgtm <elysianarchitekts@gmail.com>
2026-09-13 13:11:52 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -70,8 +70,8 @@ export default function Footer() {
             </li>
             <li>
               <a href="tel:+919535213149">+91 9535 213 149</a>
-                                          +91 9535 152 222</a>
-                                          +91 9591 977 238</a>
+              <a href="tel:+919535152222">+91 9535 152 222</a>
+              <a href="tel:+919591977238">+91 9591 977 238</a>
             </li>
             <li>
               <a href="#" style={{ lineHeight: 1.7 }}>
